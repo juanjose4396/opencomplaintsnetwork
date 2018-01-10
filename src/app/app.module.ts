89406b897af791bd4a478b7f8c077ed8e9a4d27b
Juan Jose Perdomo Forero <juanjose4396@gmail.com>
2018-01-09 19:26:13 -0500

    instalacion de bootstrap, inicializacion de ruteo, creacion de homecomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 
+const routes: Routes = [
+    {path: '' , component: HomeComponent},
+    {path: 'home', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
